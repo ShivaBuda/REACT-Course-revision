f90be22430471e8ed55056a3c52b30d0e0cfd64f
ShivBuda <shiva20073@hotmail.com>
2025-10-01 17:55:16 +0545

    add UI with styles of app

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+const App = () => {
+    return (
+        <div className="container">
+            <div className="step">
+                <button className="btn">-</button>
+                <p>Step: 0</p>
+                <button className="btn">+</button>
+            </div>
+            <div className="count">
+                <button className="btn">-</button>
+                <p>Step: 0</p>
 
-function App() {
-  const [count, setCount] = useState(0)
+                <button className="btn">+</button>
+            </div>
+            <p className="message">26 days from today is Wed Jul 21 2027</p>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
