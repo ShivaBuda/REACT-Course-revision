ae4b17551c4fd2de5804320abbac51cd663fdba0
ShivBuda <shiva20073@hotmail.com>
2025-10-01 19:37:19 +0545

    add Exercise #1

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -1,19 +1,55 @@
+import {useState} from "react";
 import "./App.css";
 const App = () => {
+    const [step, setStep] = useState(0);
+    const [count, setCount] = useState(step);
+
+    const today = new Date();
+    console.log(today.toDateString());
+    console.log(today.setDate(today.getDate() + count));
+
+    const stepPlus = function () {
+        setStep((step) => step + 1);
+    };
+    const stepMinus = function () {
+        setStep((step) => step - 1);
+    };
+
+    const dateFuture = function () {
+        setCount((count) => count + step);
+    };
+    const datePast = function () {
+        setCount((count) => count - step);
+    };
     return (
         <div className="container">
             <div className="step">
-                <button className="btn">-</button>
-                <p>Step: 0</p>
-                <button className="btn">+</button>
+                <button className="btn" onClick={stepMinus}>
+                    -
+                </button>
+                <p>Step: {step}</p>
+                <button className="btn" onClick={stepPlus}>
+                    +
+                </button>
             </div>
             <div className="count">
-                <button className="btn">-</button>
-                <p>Step: 0</p>
+                <button className="btn" onClick={datePast}>
+                    -
+                </button>
+                <p>Count: {count}</p>
 
-                <button className="btn">+</button>
+                <button className="btn" onClick={dateFuture}>
+                    +
+                </button>
             </div>
-            <p className="message">26 days from today is Wed Jul 21 2027</p>
+            <p className="message">
+                {count === 0
+                    ? "Today is "
+                    : count > 0
+                    ? `${count} days from today is `
+                    : `${Math.abs(count)} days ago was `}
+                 {today.toDateString()}
+            </p>
         </div>
     );
 };
