a7d8e9ef795ca31fdd9e813ce19e744302676115
ShivBuda <shiva20073@hotmail.com>
2025-10-03 17:34:26 +0545

    add Form section and get values from form

--- Far Away/src/Form/Form.jsx
+++ Far Away/src/Form/Form.jsx
@@ -1,6 +1,47 @@
+import {useState} from "react";
 import "./Form.css";
+
 const Form = () => {
-    return <div className="form">Form</div>;
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!description) return;
+        const newItem = {
+            id: Date.now(),
+            description,
+            quantity,
+            package: false,
+        };
+        console.log(newItem);
+
+        setDescription("");
+        setQuantity(1);
+    };
+    return (
+        <form className="form" onSubmit={handleSubmit}>
+            <h3 className="form__heading">Let's pack for your dream trip?</h3>
+            <select
+                className="form__select"
+                value={quantity}
+                onChange={(e) => setQuantity(+e.target.value)}>
+                {Array.from({length: 20}, (_, i) => i + 1).map((num) => (
+                    <option value={num} key={num}>
+                        {num}
+                    </option>
+                ))}
+            </select>
+            <input
+                type="text"
+                placeholder="Item..."
+                className="form__input"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <button className="form__btn">Add</button>
+        </form>
+    );
 };
 
 export default Form;
--- Far Away/src/PackingList/PackingList.jsx
+++ Far Away/src/PackingList/PackingList.jsx
@@ -2,17 +2,29 @@ import "./PackingList.css";
 
 const initialItems = [
     {id: 1, description: "Passport", quantity: 2, packed: false},
-    {id: 2, description: "Socks", quantity: 6, packed: false},
+    {id: 2, description: "Socks", quantity: 6, packed: true},
 ];
+
 const PackingList = () => {
     return (
         <ul className="packingList">
-            <li className="packingList__itm">
-                <input type="checkbox" name="" id="" />
-                Packinglist
-            </li>
+            {initialItems.map((item) => (
+                <Items item={item} key={item.id} />
+            ))}
         </ul>
     );
 };
+
+function Items({item}) {
+    return (
+        <li className="packingItem">
+            <input type="checkbox" />
+            <span className={item.packed ? "packedItm" : ""}>
+                {item.quantity} {item.description}
+            </span>
+            <button>&times;</button>
+        </li>
+    );
+}
 
 export default PackingList;
